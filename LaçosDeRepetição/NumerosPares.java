ab72b24de1072eeb36d350a8d5fc992a7c390e25
GiT0rres <giovanna.alvesmtorres@gmail.com>
2024-09-17 12:49:34 -0300

    Delete LaçosDeRepetição directory

--- LaçosDeRepetição/NumerosPares.java
+++
@@ -1,9 +0,0 @@
-public class NumerosPares {
-    public static void main(String[] args) {
-        for (int i = 2; i <= 300; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
-        }
-    }
-}
