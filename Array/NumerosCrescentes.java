f9cf2c8981b5558362ce65d5cdf75f193bb6e328
GiT0rres <giovanna.alvesmtorres@gmail.com>
2024-10-09 17:51:06 -0300

    Update NumerosCrescentes.java

--- Array/NumerosCrescentes.java
+++ Array/NumerosCrescentes.java
@@ -1,12 +1,17 @@
-import java.util.Arrays;
-
 public class NumerosCrescentes {
     public static void main(String[] args) {
+        // Criar um array de inteiros com 50 elementos
         int[] numeros = new int[50];
+
+        // Preencher o array com números de 1 a 50
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int) (Math.random() * 100); // Gerando números aleatórios
+            numeros[i] = i + 1; // Preenche o array com números crescentes
         }
-        Arrays.sort(numeros);
-        System.out.println("Números em ordem crescente: " + Arrays.toString(numeros));
+
+        // Exibir os números armazenados no array
+        System.out.println("Números em ordem crescente:");
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+        }
     }
 }
