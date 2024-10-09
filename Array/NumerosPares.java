b3dc79b17ccc1574a22925571df068a7713dc470
GiT0rres <giovanna.alvesmtorres@gmail.com>
2024-10-09 18:03:23 -0300

    Update NumerosPares.java

--- Array/NumerosPares.java
+++ Array/NumerosPares.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author FATEC ZONA LESTE
- */
 public class NumerosPares {
-    
+    public static void main(String[] args) {
+        int[] numerosPares = new int[100];
+        for (int i = 0; i < numerosPares.length; i++) {
+            numerosPares[i] = 2 * (i + 1); 
+        }
+        System.out.println("Primeiros 100 números pares:");
+        for (int numero : numerosPares) {
+            System.out.print(numero + " ");
+        }
+    }
 }
