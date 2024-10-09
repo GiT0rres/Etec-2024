06b2a2ddf643db5cda0cf1ace3666be49ec27ab1
GiT0rres <giovanna.alvesmtorres@gmail.com>
2024-10-09 17:51:40 -0300

    Update NumerosCrescentes.java

--- Array/NumerosCrescentes.java
+++ Array/NumerosCrescentes.java
@@ -1,14 +1,10 @@
 public class NumerosCrescentes {
     public static void main(String[] args) {
-        // Criar um array de inteiros com 50 elementos
         int[] numeros = new int[50];
-
         // Preencher o array com números de 1 a 50
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = i + 1; // Preenche o array com números crescentes
+            numeros[i] = i + 1; 
         }
-
-        // Exibir os números armazenados no array
         System.out.println("Números em ordem crescente:");
         for (int numero : numeros) {
             System.out.print(numero + " ");
