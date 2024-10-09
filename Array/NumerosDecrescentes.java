0010a8be2143742dfff06c7841ea640bef6fc02c
GiT0rres <giovanna.alvesmtorres@gmail.com>
2024-10-09 17:53:30 -0300

    Update NumerosDecrescentes.java

--- Array/NumerosDecrescentes.java
+++ Array/NumerosDecrescentes.java
@@ -1,13 +1,12 @@
-import java.util.Arrays;
-import java.util.Collections;
-
 public class NumerosDecrescentes {
     public static void main(String[] args) {
-        Integer[] numeros = new Integer[50];
+        int[] numeros = new int[50];
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int) (Math.random() * 100); // Gerando números aleatórios
+            numeros[i] = 50 - i; 
         }
-        Arrays.sort(numeros, Collections.reverseOrder());
-        System.out.println("Números em ordem decrescente: " + Arrays.toString(numeros));
+        System.out.println("Números em ordem decrescente:");
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+        }
     }
 }
